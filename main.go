f89906cee7c1789dedd5cdc8ac86e97c3564cf8a
nicktheodoro <nicolastheodoro@gmail.com>
2023-06-15 23:30:56 -0300

    Add main application logic

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"connectopia-api/src/config"
+	"connectopia-api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.Load()
+
+	fmt.Println(config.Port)
+	fmt.Println("Api running")
+
+	r := router.Generate()
+
+	fmt.Printf("Listening on port%d\n", config.Port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Port), r))
+}
