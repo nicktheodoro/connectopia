0624a711c1db2e5c73eab42d11c6e4d9cd863799
nicktheodoro <nicolastheodoro@gmail.com>
2023-06-15 23:22:39 -0300

    Add database configuration and port initialization

---
+++ src/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// DatabaseConnectionString is the MySQL connection string
+	DatabaseConnectionString = ""
+
+	// Port where the API will be running
+	Port = 0
+)
+
+// Load initializes the environment variables
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	Port, err = strconv.Atoi(os.Getenv("API_PORT"))
+	if err != nil {
+		Port = 9000
+	}
+
+	DatabaseConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
