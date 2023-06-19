ed8ad4e9fb80e4bdf6f254a00b0cf59bcccb0edb
nicktheodoro <nicolastheodoro@gmail.com>
2023-06-19 19:57:49 -0300

    Add validations on UserModel

--- src/models/UserModel.go
+++ src/models/UserModel.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"github.com/badoux/checkmail"
+)
 
 // UserModel represents a user model.
 type UserModel struct {
@@ -11,4 +17,49 @@ type UserModel struct {
 	Password  string    `json:"password,omitempty"`
 	CreatedAt time.Time `json:"createdAt,omitempty"`
 	UpdatedAt time.Time `json:"updatedAt,omitempty"`
+}
+
+// Prepare will call the methods to validate and format the received user
+func (user *UserModel) Prepare() error {
+	if err := user.validate(); err != nil {
+		return err
+	}
+
+	if err := user.format(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (user *UserModel) validate() error {
+	if user.Name == "" {
+		return errors.New("name is required and cannot be blank")
+	}
+
+	if user.Username == "" {
+		return errors.New("username is required and cannot be blank")
+	}
+
+	if user.Email == "" {
+		return errors.New("email is required and cannot be blank")
+	}
+
+	if err := checkmail.ValidateFormat(user.Email); err != nil {
+		return errors.New("the inserted email is invalid")
+	}
+
+	if user.Password == "" {
+		return errors.New("password is required and cannot be blank")
+	}
+
+	return nil
+}
+
+func (user *UserModel) format() error {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Username = strings.TrimSpace(user.Username)
+	user.Email = strings.TrimSpace(user.Email)
+
+	return nil
 }
