99d6d0967d0219153eac98c5f87390a061d8df17
nicktheodoro <nicolastheodoro@gmail.com>
2023-06-19 19:58:50 -0300

    Add repository and database connection logic in createUser method

--- src/controllers/usersController.go
+++ src/controllers/usersController.go
@@ -1,6 +1,14 @@
 package controllers
 
-import "net/http"
+import (
+	"connectopia-api/src/database"
+	"connectopia-api/src/models"
+	responses "connectopia-api/src/reponses"
+	"connectopia-api/src/repositories"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
 
 // GetUser retrieves all users.
 func GetUsers(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +22,34 @@ func GetUserByID(w http.ResponseWriter, r *http.Request) {
 
 // CreateUser handles the creation of a new user.
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Creating user!"))
+	bodyRequest, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+	}
+
+	var userModel models.UserModel
+	if err = json.Unmarshal(bodyRequest, &userModel); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+	}
+
+	if err = userModel.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+	}
+	defer db.Close()
+
+	repository := repositories.NewUsersRepository(db)
+	userModel.ID, err = repository.Create((userModel))
+	if err != nil {
+		responses.Error(w, http.StatusInternalServerError, err)
+	}
+
+	responses.JSON(w, http.StatusCreated, userModel)
 }
 
 // UpdateUser updates an existing user.
